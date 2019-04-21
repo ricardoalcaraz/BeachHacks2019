cc4eaef4c94e51e2ca1a11ad6d4cf857575537cb
Ricardo Alcaraz <ralcarazerhs@gmail.com>
2019-04-20 20:25:37 -0700

    Added an initial database

---
+++ BeachHacks/Models/Politicalparty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeachHacks.Models
+{
+    public partial class Politicalparty
+    {
+        public Politicalparty()
+        {
+            Presidentialcandidate = new HashSet<Presidentialcandidate>();
+        }
+
+        public int PoliticalPartyId { get; set; }
+        public string PartyName { get; set; }
+
+        public virtual ICollection<Presidentialcandidate> Presidentialcandidate { get; set; }
+    }
+}
---
+++ BeachHacks/Models/Tweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeachHacks.Models
+{
+    public partial class Tweet
+    {
+        public int TweetId { get; set; }
+        public string Text { get; set; }
+        public long TwitterUserId { get; set; }
+        public string TwitterName { get; set; }
+        public int PoliticalCandidate { get; set; }
+
+        public virtual Presidentialcandidate PoliticalCandidateNavigation { get; set; }
+    }
+}
