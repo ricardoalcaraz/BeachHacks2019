7cbd4ec96eb97657ba2f0984f8e4a550b42beae8
Trisha Echual <piechual@gmail.com>
2019-04-21 08:25:37 -0700

    Add DTOs

---
+++ BeachHacks/DTO/AnalysisDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace BeachHacks.DTO
+{
+    public class AnalysisDTO
+    {
+        public ICollection<EntityDTO> Entities { get; set; }
+        public ICollection<CategoryDTO> Categories { get; set; }
+    }
+}
