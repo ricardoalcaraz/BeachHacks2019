ff97e86a6022f311d190a9c8b605476ad2eeecea
Trisha Echual <piechual@gmail.com>
2019-04-21 13:53:44 -0700

    Update method to get data

--- BeachHacks/DTO/AnalysisDTO.cs
+++ BeachHacks/DTO/AnalysisDTO.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System;
 namespace BeachHacks.DTO
 {
     public class AnalysisDTO
     {
-        public ICollection<EntityDTO> Entities { get; set; }
+        //public ICollection<EntityDTO> Entities { get; set; }
         //public ICollection<CategoryDTO> Categories { get; set; }
+        public DateTime? DateOnly { get; set; }
+        public double Sentiment_Score { get; set; }
+        public double Sentiment_Mag { get; set; }
     }
 }
